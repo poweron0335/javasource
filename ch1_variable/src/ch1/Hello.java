46ce1465189a38cde46379547168f595c4471105
송민선 <poweron0335@gmail.com>
2024-01-30 17:50:19 +0900

    클래스 작성

---
+++ ch1_variable/src/ch1/Hello.java
@@ -0,0 +1,15 @@
+package ch1;
+
+// 클래스 이름은 무조건 대문자로 시작
+// 클래스명은 한글 가능하나, 웬만하면 영어로 사용
+
+public class Hello { 
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+    }
+    
+}
---
+++ ch1_variable/src/ch1/Hello2.java
@@ -0,0 +1,9 @@
+package ch1;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        int var1 = 5;
+        System.out.println("내가 좋아하는 숫자는 " + var1);
+    }
+
+}
