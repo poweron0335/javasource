72c67d3bcdcca5720dad5cf0aed2104e716ddd80
송민선 <poweron0335@gmail.com>
2024-02-02 18:18:30 +0900

    반복문, 배열

--- ch3_array/src/array/ArrayEx10.java
+++ ch3_array/src/array/ArrayEx10.java
@@ -2,6 +2,8 @@ package array;
 
 import java.util.Arrays;
 
+// 알고리즘 - 정렬
+//           선택정렬, 버블정렬, 힙정렬...etc.
 public class ArrayEx10 {
     public static void main(String[] args) {
         int arr[] = {25, 85, 55, 65, 5, 3};
